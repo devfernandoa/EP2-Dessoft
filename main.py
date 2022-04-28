877f452ebfe87361a820cb2b9bc0c400afe3e414
ilacf <ilanaf@al.insper.edu.br>
2022-04-28 19:57:06 -0300

    adicao de algumas partes do codigo principal

--- main.py
+++ main.py
@@ -4,12 +4,29 @@ from paises import base_dados_paises
 tentativas = 20
 pais = f.sorteia_pais(base_dados_paises)
 
-while tentativas > 0:
-    print('\033[0;37;40mVocê tem {} tentativas'.format(tentativas))
-    s = input('\n\033[0;37;40mQual seu chute? ')
-    if s == pais:
-        print('\n\033[0;32;40mParabéns, você acertou!')
-        break
-    else:
-        print('\n\033[0;31;40mErrou!')
-        tentativas -= 1
+jogar_dnv = "sim"
+
+while jogar_dnv == "sim":
+    while tentativas > 0:
+        print('\033[0;37;40mVocê tem {} tentativas'.format(tentativas))
+        s = input('\n\033[0;37;40mQual seu chute? ')
+        #dá uma dica para o jogador
+        if s == "dica":
+            #adicionar código das dicas
+            continue
+        #dá a resposta se o jogador desiste
+        elif s == "desisto":
+            print("Você desistiu! O país era " + pais)
+            break
+        #recebe um chute normal
+        elif s == pais:
+            print('\n\033[0;32;40mParabéns, você acertou!')
+            break
+        #recebe um chute inválido
+        elif s not in base_dados_paises.keys():
+            print('chute inválido')
+        else:
+            print('\n\033[0;31;40mErrou!')
+            tentativas -= 1
+    #pergunta se o jogador quer reiniciar o jogo
+    jogar_dnv = print((input("Você quer jogar de novo?")).lower)
