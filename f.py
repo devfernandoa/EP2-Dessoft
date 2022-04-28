e1ab41b938ed5091b4a0b8f59311e57bdfff40d6
ilacf <ilanaf@al.insper.edu.br>
2022-04-28 15:53:38 -0300

    funcao haversine

--- f.py
+++ f.py
@@ -19,3 +19,8 @@ def sorteia_pais(nomes_de_paises):
         paises.append(i)
     pais = random.choice(paises)
     return pais
+
+#calcula a distância entre dois países usando a fórmula de Haversine
+def haversine(r, p1, l1, p2, l2): 
+    d = 2 * r * asin(sqrt(sin(radians((p2-p1)/2)) ** 2 + (cos(radians(p1)) * cos(radians(p2)) * (sin(radians((l2-l1)/2)) ** 2))))
+    return d
