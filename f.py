2e94bff82f38a9d36a254b07594e78c7c7d223f2
ilacf <ilanaf@al.insper.edu.br>
2022-04-28 16:19:24 -0300

    funcao adiciona_em_ordem

--- f.py
+++ f.py
@@ -20,7 +20,22 @@ def sorteia_pais(nomes_de_paises):
     pais = random.choice(paises)
     return pais
 
-#calcula a distância entre dois países usando a fórmula de Haversine
+#função que calcula a distância entre dois países usando a fórmula de Haversine
 def haversine(r, p1, l1, p2, l2): 
     d = 2 * r * asin(sqrt(sin(radians((p2-p1)/2)) ** 2 + (cos(radians(p1)) * cos(radians(p2)) * (sin(radians((l2-l1)/2)) ** 2))))
     return d
+
+#função que adiciona os países na lista com distâncias em ordem crescente
+def adiciona_em_ordem(nome, distancia, lista):
+    if lista == []:
+        return [[nome, distancia]]
+    elif len(lista) == 1:
+        if distancia > lista[0][1]:
+            lista.append([nome, distancia])
+        else:
+            lista.insert(0, [nome, distancia])
+    else:
+        for i in range(len(lista)):
+            if (distancia > lista[i][1]) and (distancia < lista[i+1][1]):
+                lista.insert(i+1, [nome, distancia])
+    return lista
