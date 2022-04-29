772aadb19f32626469b0f764a04a912fba5fdcf7
devfernandoa <fefealzueta@gmail.com>
2022-04-29 08:13:14 -0300

    Adicionada classe de cores
    Adicionado um arquivo com uma classe para facilitar a coloração de strings, também foram corrigidos alguns bugs, como as variáveis iniciais estarem sendo declaradas fora do loop

---
+++ cores.py
@@ -0,0 +1,9 @@
+class cor:
+   preto = '\033[0;37;40m'
+   roxo = '\033[95m'
+   azul = '\033[94m'
+   verde = '\033[92m'
+   amarelo = '\033[93m'
+   vermelho = '\033[91m'
+   negrito = '\033[1m'
+   fim = '\033[0m'
--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 import f
 from paises import base_dados_paises
+from cores import cor
 
-tentativas = 20
-pais = f.sorteia_pais(base_dados_paises)
+raio = 6371
 
 jogar_dnv = "sim"
 
 while jogar_dnv == "sim":
+
+    tentativas = 20
+    lista_chutes = []
+    lista_paises = []
+
     while tentativas > 0:
-        print('\033[0;37;40mVocê tem {} tentativas'.format(tentativas))
-        s = input('\n\033[0;37;40mQual seu chute? ')
+        print('\n' + cor.preto  + cor.negrito + 'Você tem {} tentativas'.format(tentativas) + cor.fim)
+        s = input('\n' + cor.preto  + cor.negrito + 'Qual seu chute? ' + cor.fim)
+
         #dá uma dica para o jogador
         if s == "dica":
             #adicionar código das dicas
             continue
+
         #dá a resposta se o jogador desiste
         elif s == "desisto":
             print("Você desistiu! O país era " + pais)
             break
         #recebe um chute normal
-        elif s == pais:
-            print('\n\033[0;32;40mParabéns, você acertou!')
+        if s == pais:
+            print('\n' + cor.verde + cor.negrito + 'Parabéns, você acertou!' + cor.fim)
             break
+
         #recebe um chute inválido
         elif s not in base_dados_paises.keys():
             print('chute inválido')
         else:
-            print('\n\033[0;31;40mErrou!')
+            print('\n' + cor.vermelho + 'Você errou!\n' + cor.fim)
+
             tentativas -= 1
     #pergunta se o jogador quer reiniciar o jogo
     jogar_dnv = print((input("Você quer jogar de novo?")).lower)
