9b2f772188db10dbe4a7855f031a01de52b90823
ilacf <ilanaf@al.insper.edu.br>
2022-05-01 15:41:39 -0300

    algumas correcoes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from cores import cor
 raio = 6371
 jogar_dnv = "sim"
 
-while jogar_dnv == "sim":
+while jogar_dnv == "Sim".lower():
 
     #print da introdução
     print('Bem vindo ao Insper Paises, o projeto 2 de Design de Software feito por Fernando Alzueta e Ilana Chaia Finger\nAqui seu objetivo é acertar um país sorteado pelo computador, você terá 20 chances para acertar\nA cada tentativa você pode comprar dicas, chutar um país ou desistir\nSe você chutar um país, ele será adicionado em uma lista que possui a distancia entre a capital do pais sorteado e do chute\n')
@@ -18,31 +18,34 @@ while jogar_dnv == "sim":
     p1 = base_dados_paises[pais]['geo']['latitude']
     l1 = base_dados_paises[pais]['geo']['longitude']
 
+    #outras variáveis básicas
     tentativas = 20
-    lista_chutes = []
     lista_paises = []
 
+    #loop principal
     while tentativas > 0:
         print('\n' + cor.preto  + cor.negrito + 'Você tem {} tentativas'.format(tentativas) + cor.fim)
+
+        #recebe o input do jogador
         chute = input('\n' + cor.preto  + cor.negrito + 'Qual seu chute? ' + cor.fim)
 
         #dá uma dica para o jogador
-        if chute == "dica":
+        if chute == "Dica".lower():
             #adicionar código das dicas
             continue
 
         #dá a resposta se o jogador desiste
-        elif chute == "desisto":
+        elif chute == "Desisto".lower():
             print("Você desistiu! O país era " + pais)
             break
 
         #mostra os comandos
-        elif chute == "comandos":
+        elif chute == "Comandos".lower():
             print('Comandos: \ndica - entra no mercado de dicas se você ainda tiver dicas disponíveis\ndesisto - sai do programa\ncomandos - exibe os comandos\nchutes - exibe a lista de paises chutados')
             continue
 
         #mostra a lista de países chutados até o momento
-        elif chute == "chutes":
+        elif chute == "Chutes".lower():
             print(lista_chutes)
 
         #recebe um chute válido correto
@@ -54,17 +57,19 @@ while jogar_dnv == "sim":
         elif chute not in base_dados_paises.keys():
             print('chute inválido')
             continue
-        elif chute in lista_chutes:
+
+        #recebe um país que já foi chutado
+        elif chute in lista_paises:
             print('você já chutou esse pais')
             continue
-        lista_chutes.append(chute)
 
-        #declarando latitude e longitude do pais do chute
+        #declarando latitude e longitude do pais do chute para calcular distância desse país até o país sorteado
         p2 = base_dados_paises[chute]['geo']['latitude']
         l2 = base_dados_paises[chute]['geo']['longitude']
 
-        #calculando distancia entre os dois paises
+        #calculando distância entre os dois paises
         d = round(f.haversine(raio, p1, l1, p2, l2))
+
         #recebe um chute válido errado
         if chute != pais:
             print('\n' + cor.vermelho + 'Você errou!\n' + cor.fim)
@@ -73,7 +78,7 @@ while jogar_dnv == "sim":
             lista_paises = f.adiciona_em_ordem(chute, d, lista_paises)
 
             #mostra a lista de paises e distâncias
-            print('distancias: ')
+            print('Distâncias entre os chutes e o país sorteado: ')
             for i in lista_paises:
                 if i[1] > 10000:
                     print(cor.roxo + 'pais -> ', i[0], '\ndistancia -> ', str(i[1]) + cor.fim)
@@ -85,4 +90,4 @@ while jogar_dnv == "sim":
                     print(cor.verde + 'pais -> ', i[0], '\ndistancia -> ', str(i[1]) + cor.fim)
             tentativas -= 1
     #pergunta se o jogador quer reiniciar o jogo
-    jogar_dnv = input("Você quer jogar de novo?").lower()
+    jogar_dnv = input("Você quer jogar de novo?")
